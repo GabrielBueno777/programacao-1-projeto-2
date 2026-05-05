58c6ae96a4d89f174cdb4a90b3e1341abd2efc14
GabrielBueno777 <gabrielbueno150809@gmail.com>
2026-05-05 14:33:46 -0300

    Add files via upload

---
+++ programacao1_att2/index.js
@@ -0,0 +1,49 @@
+const containerCadastrar = document.querySelector('.cadastrar')
+const inputTarefa = containerCadastrar.querySelector('input')
+const btnAdicionar = containerCadastrar.querySelector('button')
+const containerTarefas =document.querySelector('.tarefas')
+const templateTarefa = containerTarefas.querySelector('template')
+
+function carregarTarefas(){
+    const stringTarefas = localStorage.getItem('tarefas')
+    const arrayTarefas = JSON.parse(stringTarefas) || []
+    arrayTarefas.forEach(criarTarefa) 
+}
+
+function salvarTarefas() {
+    const nodeListTarefas = containerTarefas.querySelectorAll(':scope > .tarefa span')
+    const arrayTarefas = Array.from(nodeListTarefas).map(el => el.textContent)
+    const stringTarefas = JSON.stringify(arrayTarefas)
+    localStorage.setItem('tarefas', stringTarefas)
+}
+
+function criarTarefa(texto) {
+    if(texto.trim() === ' ') return
+    const tarefa = templateTarefa.content.cloneNode(true)
+    const spanTtile = tarefa.querySelector('span')
+    const btnExcluir = tarefa.querySelector('button')
+    spanTtile.textContent = texto
+    btnExcluir.addEventListener('click', () => {
+        btnExcluir.closest('.tarefa').remove()
+        salvarTarefas()
+    })
+    containerTarefas.appendChild(tarefa)
+    salvarTarefas()
+}
+
+btnAdicionar.addEventListener('click',  () => {
+    const texto = inputTarefa.value.trim()
+    criarTarefa(texto)
+    inputTarefa.value = ' '
+})
+
+inputTarefa.addEventListener('keypress', (Event) => {
+    if(Event.key !== 'Enter') return
+    btnAdicionar.click()
+})
+inputTarefa.addEventListener('keypress', (event) => {
+    if(event.key !== 'Backspace') return
+    btnExcluir.click()
+})
+
+carregarTarefas()
